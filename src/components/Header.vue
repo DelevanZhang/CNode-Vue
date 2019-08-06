cf0fa5420a139a7fe1d57da218c99128af2d089f
Delevan <523373965@qq.com>
2019-08-06 16:45:43 +0800

    主题完成一小部分

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,61 +1,74 @@
 <template>
-  <div class="container">
-      <img src="../assets/cnodejs_light.svg" alt="picture">
-      <ul >
-          <li><a href="#">首页</a></li>
-          <li><a href="#">新手入门</a></li>
-          <li><a href="#">API</a></li>
-          <li><a href="#">关于</a></li>
-          <li><a href="#">注册</a></li>
-          <li><a href="#">登录</a></li>
+  <div class="Header">
+    <div class="container">
+      <img src="../assets/cnodejs_light.svg" alt="picture" />
+      <ul>
+        <li>
+          <a href="#">首页</a>
+        </li>
+        <li>
+          <a href="#">新手入门</a>
+        </li>
+        <li>
+          <a href="#">API</a>
+        </li>
+        <li>
+          <a href="#">关于</a>
+        </li>
+        <li>
+          <a href="#">注册</a>
+        </li>
+        <li>
+          <a href="#">登录</a>
+        </li>
       </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-
-    }
+    return {};
   },
-  components: {
-
-  }
-}
+  components: {}
+};
 </script>
 
 <style scoped>
-.container {
-    width:100%;
-    height: 50px;
-    background-color: #3D3D3D;
-    position: relative;
+.Header {
+  width: 100%;
+  height: 50px;
+  background-color: #3d3d3d;
+}
+
+.Header > .container {
+  max-width: 1400px;
+  position: relative;
+  height: 100%;
+  margin: 0 auto;
 }
 ul {
-    position:absolute;
-    height:50px;
-    top:0;
-    right:14%;
-    margin:0;
-    padding: 0;
-    line-height: 50px;
-
+  position: absolute;
+  height: 50px;
+  top: 0;
+  right: 0;
+  margin: 0;
+  padding: 0;
+  line-height: 50px;
 }
 li {
-    list-style: none;
-    display:inline-block;
-    padding: 0 15px;
+  list-style: none;
+  display: inline-block;
+  padding: 0 15px;
 }
 a {
-    text-decoration: none;
-    color: #ccc;
-    font-size: 13px;
+  text-decoration: none;
+  color: #ccc;
+  font-size: 13px;
 }
 img {
-    width:6%;
-    height: 100%;
-    margin-left:13%;
+  width: 120px;
+  height: 100%;
 }
-
 </style>
--- src/router.js
+++ src/router.js
@@ -5,20 +5,20 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Header,
-    },
-    {
-      path: '/about',
-      name: 'about',
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: Header,
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
-    }
+    //   component: function () { 
+    //     return import(/* webpackChunkName: "about" */ './views/About.vue')
+    //   }
+    // }
   ]
 })
